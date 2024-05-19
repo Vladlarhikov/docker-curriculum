9d2e883a570bc9ce14eda299d1968addc871017c
Vladlarhikov <160498888+Vladlarhikov@users.noreply.github.com>
2024-05-19 20:29:36 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+print("Вычислим, сколько дней, минут и секунд вы живете.")
+
+name = input("имя: ")
+
+print("Введите свой возраст")
+age = int(input("возраст: "))
+
+days = age * 365
+minutes = age * 5285948
+seconds = age * 31556926
+
+print(name, "прожил(а)", days, "дней", minutes, "минут и", seconds, "секунд!")
+
